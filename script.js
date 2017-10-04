1c9345f45d590b11dfb349a79e11bce390172354
Rafael Prado Santos <rps4@ug3c10.windows.cin.ufpe.br>
2017-10-04 16:06:26 -0300

    Navbar pronta

---
+++ script.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+function resizeCanvas(width, height) {
+    canvas.width = width;
+    canvas.height = height;
+}
+
+function resizeToFit() {
+    var width = parseFloat(window.getComputedStyle(canvas).width);
+    var height = parseFloat(window.getComputedStyle(canvas).height);
+    resizeCanvas(width, height);
+}
