94cc7d9fcfecb79e65b0d32736b6cacab859440f
Rafael Prado Santos <rps4@ug2c41.windows.cin.ufpe.br>
2017-10-24 15:36:20 -0300

    curvaa

--- script.js
+++ script.js
@@ -7,34 +7,48 @@ function resizeCanvas() {
 }
 
 function decas(){
+  curves=[];
+  aux=[];
   for(var i=0; i<tam;i++){
-    aux=points.slice();
+    for (var j in points){
+      aux.push(points[j]);
+    }
     curves.push(tejau(i));
   }
 }
 
 function tejau(para){
-  if (aux.length=1){
+  if (aux.length==1){
     return aux[0];
+    alert(aux[0]);
   }
   else{
     var aux1=[];
       for(var j in aux){
-        if(i!=0){
-          aux1.push({x: aux[j].x - (aux[j-1].x*(para/tam)), x: aux[j].y- (aux[j-1].y*(para/tam))});
+        if(j!=0){
+          aux1.push({x: (aux[j-1].x*(para/tam)) + (aux[j].x*(1-para/tam)), y: (aux[j-1].y*(para/tam)) + (aux[j].y*(1-para/tam))});
         }
-        aux=aux1;
       }
+    aux=[];
+    for (var j in aux1){
+      aux.push(aux1[j]);
+    }
+    return tejau(para);
   }
 }
 
 function drawCircles() {
   ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
   for (var i in points) {
     ctx.beginPath();
-    ctx.arc(points[i].x, points[i].y, 3, 0, 2 * Math.PI);
+    ctx.arc(points[i].x, points[i].y, 6, 0, 2 * Math.PI);
     ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(points[i].x, points[i].y, 4, 0, 2 * Math.PI);
+    ctx.fillStyle = 'white';
     ctx.fill();
   }
 
@@ -43,22 +57,21 @@ function drawCircles() {
   for (var i in points) {
     
     if(i!=0){
-    ctx.beginPath();
-    ctx.strokeStyle = 'black'; //Cor do traço
-    ctx.lineWidth = 2; //Espessura do traço
-    ctx.moveTo(x,y); //Move o Ponteiro
-    ctx.lineTo(x+points[i].x-points[i-1].x,y+points[i].y-points[i-1].y);
-    x=x+points[i].x-points[i-1].x;
-    y=y+points[i].y-points[i-1].y;
-    ctx.stroke();
+      ctx.beginPath();
+      ctx.strokeStyle = 'black'; //Cor do traço
+      ctx.lineWidth = 2; //Espessura do traço
+      ctx.moveTo(x,y); //Move o Ponteiros
+      ctx.lineTo(points[i].x,points[i].y);
+      x=points[i].x;
+      y=points[i].y;
+      ctx.stroke();
     }
   }
 
-  decas();
 
   for (var i in curves) {
     ctx.beginPath();
-    ctx.arc(curves[i].x, curves[i].y, 1, 0, 2 * Math.PI);
+    ctx.arc(curves[i].x, curves[i].y, 3, 0, 2 * Math.PI);
     ctx.fillStyle = 'black';
     ctx.fill();
   }
@@ -66,19 +79,17 @@ function drawCircles() {
   x=curves[0].x;
   y=curves[0].y;
   for (var i in curves) {
-    
     if(i!=0){
-    ctx.beginPath();
-    ctx.strokeStyle = 'black'; //Cor do traço
-    ctx.lineWidth = 2; //Espessura do traço
-    ctx.moveTo(x,y); //Move o Ponteiro
-    ctx.lineTo(x+curves[i].x-curves[i-1].x,y+curves[i].y-curves[i-1].y);
-    x=x+curves[i].x-curves[i-1].x;
-    y=y+curves[i].y-curves[i-1].y;
-    ctx.stroke();
+      ctx.beginPath();
+      ctx.strokeStyle = 'black'; //Cor do traço
+      ctx.lineWidth = 2; //Espessura do traço
+      ctx.moveTo(x,y); //Move o Ponteiro
+      ctx.lineTo(curves[i].x,curves[i].y);
+      x=curves[i].x
+      y=curves[i].y;
+      ctx.stroke();
     }
   }
-
 }
 
 function dist(p1, p2) {
@@ -88,7 +99,7 @@ function dist(p1, p2) {
 
 function getIndex(click) {
   for (var i in points) {
-    if (dist(points[i], click) <= 10) {
+    if (dist(points[i], click) <= 5) {
       return i;
     }
   }
@@ -100,7 +111,7 @@ var curves = [];
 var aux =[];
 var move = false;
 var index = -1;
-var tam=1000;
+var tam=100;
 
 resizeCanvas();
 
@@ -109,6 +120,7 @@ canvas.addEventListener('mousedown', e => {
   index = getIndex(click);
   if (index === -1) {
     points.push(click);
+    decas();
     drawCircles();
   } else {
     move = true;
@@ -118,6 +130,7 @@ canvas.addEventListener('mousedown', e => {
 canvas.addEventListener('mousemove', e => {
   if (move) {
     points[index] = {x: e.offsetX, y: e.offsetY};
+    decas();
     drawCircles();
   }
 });
@@ -129,10 +142,12 @@ canvas.addEventListener('mouseup', e => {
 canvas.addEventListener('dblclick', e => {
   if (index !== -1) {
     points.splice(index, 1);
+    decas();
     drawCircles();
   }
 });
 
 setInterval(() => {
-  drawCircles();
-}, 1000 / 30);
+  if(!move)
+    drawCircles();
+}, 1/ 3000);
